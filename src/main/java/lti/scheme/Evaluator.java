83a62ddbb7d3067ae046bdd1a3c63b9129c70fb9
LTI2000 <timo.koepke@googlemail.com>
2026-02-19 23:07:55 +0100

    Implement eval for Conditional

--- src/main/java/lti/scheme/Evaluator.java
+++ src/main/java/lti/scheme/Evaluator.java
@@ -4,12 +4,23 @@ import lti.scheme.Expression.*;
 import lti.scheme.Value.*;
 
 public final class Evaluator {
-  public Value eval(Expression exp, Environment env, Continuation<Value,Value> k) {
+  public Value eval(Expression exp, Environment env, Continuation<Value, Value> k) {
     return switch (exp) {
-      case Quotation(var value) -> value;
-      case Variable(var name) -> env.lookup(name);
-      case Abstraction abstraction -> null;
-      case Conditional conditional -> null;
+      case Quotation(var value) ->
+        k.apply(value);
+      case Variable(var name) ->
+        k.apply(env.lookup(name));
+      case Conditional(var test, var consequent, var alternate) ->
+        eval(test, env, t -> isTrue(t) ? eval(consequent, env, k) : eval(alternate, env, k));
+      case Abstraction abstraction ->
+        null;
+    };
+  }
+
+  private boolean isTrue(Value value) {
+    return switch (value) {
+      case Bool(var bool) when !bool -> false;
+      default -> true;
     };
   }
 }
--- src/main/java/lti/scheme/Value.java
+++ src/main/java/lti/scheme/Value.java
@@ -3,4 +3,7 @@ package lti.scheme;
 sealed interface Value {
   record Symbol(String name) implements Value {
   }
+
+  record Bool(boolean bool) implements Value {
+  }
 }
